f96cfc40976abc4e96636a73bee585790c05b50c
koluku <korufiru@gmail.com>
2016-12-13 15:22:27 +0900

    Python版

---
+++ weather.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+def get_json(url):
+    res = requests.get(url)
+    json = res.json()
+    return json
+
+def post_to_slack(message):
+    data = json.dumps({
+        'text': message,
+        'username': 'weather_bot',
+    })
+    requests.post(WEBHOOK, data)
+
+def main():
+    url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=017010'
+    json = get_json(url)
+    date = json['forecasts'][1]['date']
+    weather = json['forecasts'][1]['telop']
+    max_temperature = json['forecasts'][1]['temperature']['max']['celsius']
+    min_temperature = json['forecasts'][1]['temperature']['min']['celsius']
+    post_to_slack(date + 'の天気予報\n' + weather + '\n最高気温 '  + max_temperature + '℃ / 最低気温 ' + min_temperature + '℃')
+
+if __name__ == '__main__':
+    main()
